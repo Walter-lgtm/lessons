3a69010d834d524e4591923835e3c6a7b320dbbe
Walter-lgtm <dionis.pecherskih21@gmail.com>
2026-09-17 20:05:07 +0700

    Update script.js

--- bio/8/ls/06/script.js
+++ bio/8/ls/06/script.js
@@ -123,7 +123,6 @@ document.addEventListener('DOMContentLoaded', () => {
                 feedbackDiv.textContent = `❌ Нет ответа. Правильный ответ: ${CORRECT_ANSWERS[`q\${i}`]}`;
                 feedbackDiv.classList.add('error-text');
             }
-            {
 
         // Шкала оценок для 5 вопросов: 5 верных = "5", 4 верных = "4", 3 верных = "3", меньше = "2"
         let grade = 2;
